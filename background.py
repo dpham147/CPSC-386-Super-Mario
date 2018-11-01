2ed7b2bcbf9928f0ee4cc474dc19f1dce1397aeb
lw27 <w793470601@gmail.com>
2018-10-31 18:36:28 -0700

    update

--- background.py
+++ background.py
@@ -1,2 +1,9 @@
-class Background():
+import pygame
+import MarioClass from mario
+
+class Background:
     def __init__(self):
+        self.image = pygame.image.load("'/images/1-1.png")
+        self.rect = self.image.rect
+        self.centerx = self.rect.x
+        self.centery = self.rect.y
