34f565e33c11c25648adc12d7a8fa4b6d785dc14
lw27 <w793470601@gmail.com>
2018-11-05 17:31:01 -0800

    Update background.py
    update

--- background.py
+++ background.py
@@ -3,10 +3,15 @@ import pygame
 
 class Background:
     def __init__(self):
-        self.image = pygame.image.load("'/images/1-1.png")
-        self.rect = self.image.rect
-        self.centerx = self.rect.x
-        self.centery = self.rect.y
+        self.image = pygame.image.load('images/1-1.png')
+        self.rect = self.image.get_rect()
+        self.screen_rect = self.screen.get_rect()
+        self.mario_rect = self.mario.get_rect()
+        self.rect.centerx = self.screen_rect.centerx
+        self.center = float(self.rect.centerx)
+        self.mario_moving_right = False
 
     def update(self):
-        if mario_position > self.centerx:
+        if self.mario_moving_right and self.mario_rect.centerx > self.rect.centerx:
+            #scroll background
+
