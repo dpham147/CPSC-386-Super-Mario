206df27e81e2a0bafd96a9aa5888b8c0bf9f4145
Danny Pham <dannypham92@hotmail.com>
2018-11-07 23:55:06 -0800

    Direct Upload
    Push rejected from Pycharm

--- eventloop.py
+++ eventloop.py
@@ -1,6 +1,27 @@
 import pygame
+import sys
 
 
 class EventLoop:
     def __init__(self, finished):
         self.finished = finished
+
+    def check_events(self, mario):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    mario.jump()
+                elif event.key == pygame.K_LSHIFT:
+                    print("Pew pew")
+                    # Mario fireball
+                elif event.key == pygame.K_d:
+                    mario.mv_right = True
+                elif event.key == pygame.K_s:
+                    print("Crouch")
+                    # Mario crouch
+                elif event.key == pygame.K_a:
+                    mario.mv_right = True
+                elif event.key == pygame.K_q:
+                    sys.exit()
--- game.py
+++ game.py
@@ -1,18 +1,35 @@
 import pygame
 from eventloop import EventLoop
 from background import Background
+from mario import Mario
 
 
 class Game:
 
     def __init__(self):
+        # Init the display
         pygame.init()
-        self.screen = pygame.display.set_mode(pygame.FULLSCREEN)
+        self.screen = pygame.display.set_mode((700, 400))
         pygame.display.set_caption("Super Mario")
+
+        # Create Mario
+        self.mario = Mario(self.screen)
+
+        # Create the level
+        self.background = Background(self.screen, self.mario)
 
     def play(self):
         eloop = EventLoop(finished=False)
+        while not eloop.finished:
+            eloop.check_events(self.mario)
+            self.update_screen()
 
     def update_screen(self):
-        self.screen.fill(0, 0, 0)
+        self.screen.fill((0, 0, 0))
         self.background.blitme()
+        self.mario.blitme()
+        pygame.display.flip()
+
+
+game = Game()
+game.play()
