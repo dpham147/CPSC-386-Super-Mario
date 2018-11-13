939dd68b70dac40daee7669eb4a30ff7b2535f81
lw27 <w793470601@gmail.com>
2018-11-12 23:43:01 -0800

    update floor

--- floor.py
+++ floor.py
@@ -2,7 +2,6 @@ import pygame
 from pygame.sprite import Sprite
 
 
-
 class Floor(Sprite):
     def __init__(self, screen):
         super(Floor, self).__init__()
@@ -10,7 +9,6 @@ class Floor(Sprite):
         self.screen_rect = self.screen.get_rect()
         self.image = pygame.image.load('images/floor.png')
         self.rect = self.image.get_rect()
-
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
